1dcc16025c2ac3bdcbd2c95297e60aaa08c6cc3c
hbam1 <hbbaamm@gmail.com>
2024-01-02 23:01:22 +0900

    chap1-7. handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -7,7 +7,7 @@ function loadItems() {
 }
 
 // Update the list with the given items
-function displayItems(items) {
+function displayItems(items) { 
     const container = document.querySelector(".items");
     container.innerHTML = items.map(item => createHTMLString(item)).join("");
 }
@@ -20,6 +20,25 @@ function createHTMLString(item) {
             <span class="item__description">${item.gender}, ${item.size}</span>
         </li>
     `;
+}
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+
+    if (key === null || value === null) {
+        return;
+    }
+
+    displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+    const logo = document.querySelector(".logo");
+    const buttons = document.querySelector(".buttons");
+    logo.addEventListener("click", () => displayItems(items));
+    buttons.addEventListener("click", event => onButtonClick(event, items));
 }
 
 // main
