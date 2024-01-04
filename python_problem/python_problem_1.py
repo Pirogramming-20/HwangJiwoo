bc520b540f105ff2ffa107019ec754f1fb78656b
hbam1 <hbbaamm@gmail.com>
2024-01-05 02:20:20 +0900

    python_problem_1_6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,35 +1,42 @@
 num = 0
 # 부를 숫자의 수
 cnt = 0
+# 게임을 계속할 지를 결정
+flag = True
 
-while True:
-  user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-  try:
-      user_input = int(user_input)
-      if user_input in (1, 2, 3):
-          cnt = user_input
-          break
-      else:
-          print("1,2,3 중 하나를 입력하세요")
-  except ValueError:
-      print("정수를 입력하세요")
-
-for i in range(cnt):
-  num = num + 1
-  print("playerA :", num)
-
-while True:
-  user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-  try:
-      user_input = int(user_input)
-      if user_input in (1, 2, 3):
-          cnt = user_input
-          break
-      else:
-          print("1,2,3 중 하나를 입력하세요")
-  except ValueError:
-      print("정수를 입력하세요")
-
-for i in range(cnt):
-  num = num + 1
-  print("playerB :", num)
+while flag:
+  while flag:
+    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    try:
+        user_input = int(user_input)
+        if user_input in (1, 2, 3):
+            cnt = user_input
+            for i in range(cnt):
+              num = num + 1
+              print("playerA :", num)
+              if num == 31:
+                flag = False
+                break
+            break
+        else:
+            print("1,2,3 중 하나를 입력하세요")
+    except ValueError:
+        print("정수를 입력하세요")
+  
+  while flag:
+    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+    try:
+        user_input = int(user_input)
+        if user_input in (1, 2, 3):
+            cnt = user_input
+            for i in range(cnt):
+              num = num + 1
+              print("playerB :", num)
+              if num == 31:
+                flag = False
+                break
+            break
+        else:
+            print("1,2,3 중 하나를 입력하세요")
+    except ValueError:
+        print("정수를 입력하세요")
