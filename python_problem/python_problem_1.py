6b0b9d66fbb5c5832eea309b4ee0961dc928705e
hbam1 <hbbaamm@gmail.com>
2024-01-05 01:58:43 +0900

    python_problem_1_5단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -17,3 +17,19 @@ while True:
 for i in range(cnt):
   num = num + 1
   print("playerA :", num)
+
+while True:
+  user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
+  try:
+      user_input = int(user_input)
+      if user_input in (1, 2, 3):
+          cnt = user_input
+          break
+      else:
+          print("1,2,3 중 하나를 입력하세요")
+  except ValueError:
+      print("정수를 입력하세요")
+
+for i in range(cnt):
+  num = num + 1
+  print("playerB :", num)
