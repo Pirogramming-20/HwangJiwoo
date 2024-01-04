9aef129e4963c763dab274fb79b451801d1313d2
hbam1 <hbbaamm@gmail.com>
2024-01-05 02:51:18 +0900

    python_problem_1_8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -3,9 +3,11 @@ num = 0
 cnt = 0
 # 게임을 계속할 지를 결정
 flag = True
+# 차례
+player = "playerA"
 
-while flag:
-  while flag:
+def br_Game(player, num):
+  while True:
     user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
     try:
       user_input = int(user_input)
@@ -13,32 +15,20 @@ while flag:
         cnt = user_input
         for i in range(cnt):
           num = num + 1
-          print("playerA :", num)
+          print(player + " :", num)
           if num == 31:
-            flag = False
-            print("playerB win!")
-            break
-        break
+            return num
+        return num
       else:
         print("1,2,3 중 하나를 입력하세요")
     except ValueError:
       print("정수를 입력하세요")
 
-  while flag:
-    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-    try:
-      user_input = int(user_input)
-      if user_input in (1, 2, 3):
-        cnt = user_input
-        for i in range(cnt):
-          num = num + 1
-          print("playerB :", num)
-          if num == 31:
-            flag = False
-            print("playerA win!")
-            break
-        break
-      else:
-        print("1,2,3 중 하나를 입력하세요")
-    except ValueError:
-      print("정수를 입력하세요")
+while num < 31:
+  num = br_Game(player, num)
+  if (player == "playerA"):
+    player = "playerB"
+  else:
+    player = "playerA"
+
+print(player + " win!")
