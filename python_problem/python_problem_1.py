36c2f055150731aa094e74140bb4e45cb69fe298
hbam1 <hbbaamm@gmail.com>
2024-01-05 03:06:47 +0900

    python_problem_1_9단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,34 +1,42 @@
+from random import randint
+
 num = 0
-# 부를 숫자의 수
-cnt = 0
-# 게임을 계속할 지를 결정
-flag = True
 # 차례
-player = "playerA"
+player = "computer"
 
 def br_Game(player, num):
-  while True:
-    user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) :")
-    try:
-      user_input = int(user_input)
-      if user_input in (1, 2, 3):
-        cnt = user_input
-        for i in range(cnt):
-          num = num + 1
-          print(player + " :", num)
-          if num == 31:
-            return num
+  if player == "computer":
+    computer_input = randint(1, 3)
+    cnt = computer_input
+    for i in range(cnt):
+      num += 1
+      print(player + " :", num)
+      if num == 31:
         return num
-      else:
-        print("1,2,3 중 하나를 입력하세요")
-    except ValueError:
-      print("정수를 입력하세요")
+    return num
+  else:
+    while True:
+      user_input = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+      try:
+        user_input = int(user_input)
+        if user_input in (1, 2, 3):
+          cnt = user_input
+          for i in range(cnt):
+            num = num + 1
+            print(player + " :", num)
+            if num == 31:
+              return num
+          return num
+        else:
+          print("1,2,3 중 하나를 입력하세요")
+      except ValueError:
+        print("정수를 입력하세요")
 
 while num < 31:
   num = br_Game(player, num)
-  if (player == "playerA"):
-    player = "playerB"
+  if (player == "computer"):
+    player = "player"
   else:
-    player = "playerA"
+    player = "computer"
 
 print(player + " win!")
